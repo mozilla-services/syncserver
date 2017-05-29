6e5f07667f2d4d7435cb37a7638d2a7ed4ed3e35
Ryan Kelly <rfkelly@mozilla.com>
2017-05-29 13:41:03 +1000

    feat(verifier):  Use the newly-enhanced LocalVerifier class by default.

--- syncserver/__init__.py
+++ syncserver/__init__.py
@@ -97,10 +97,10 @@ def includeme(config):
     if "storage.batch_upload_enabled" not in settings:
         settings["storage.batch_upload_enabled"] = False
     if "browserid.backend" not in settings:
-        # Default to remote verifier for simplicity.
+        # Default to local verifier to reduce external dependencies.
         # Use base of public_url as only audience
         audience = urlunparse(urlparse(public_url)._replace(path=""))
-        settings["browserid.backend"] = "tokenserver.verifiers.RemoteVerifier"
+        settings["browserid.backend"] = "tokenserver.verifiers.LocalVerifier"
         settings["browserid.audiences"] = audience
     if "loggers" not in settings:
         # Default to basic logging config.
