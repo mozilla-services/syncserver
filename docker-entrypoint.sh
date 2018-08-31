a9a8d66bcdf26fcd1d3cc38af1524eb1fbac9047
John Morrison <jrgmorrison@gmail.com>
2018-08-31 16:12:15 -0700

    [docker-entry-point.sh] add missing backslash line continuation (#133)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ case "$1" in
     server)
         export SYNCSERVER_SQLURI="${SYNCSERVER_SQLURI:-sqlite:///tmp/syncserver.db}"
         exec gunicorn \
-            --bind ${HOST-0.0.0.0}:${PORT-5000}\
-            --forwarded-allow-ips="${SYNCSERVER_FORWARDED_ALLOW_IPS:-127.0.0.1,172.17.0.1}"
+            --bind ${HOST-0.0.0.0}:${PORT-5000} \
+            --forwarded-allow-ips="${SYNCSERVER_FORWARDED_ALLOW_IPS:-127.0.0.1,172.17.0.1}" \
             syncserver.wsgi_app
         ;;
 
